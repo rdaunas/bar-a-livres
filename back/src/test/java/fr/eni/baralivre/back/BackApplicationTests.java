30947927c211c375743a7687a452d9531c1bf837
rdaunas <romain.daunas@gmail.com>
2026-04-07 10:52:27 +0200

    Init projet spring

---
+++ back/src/test/java/fr/eni/baralivre/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.baralivre.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
