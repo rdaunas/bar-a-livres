0d367fcb364f2f4e833189fcb3e753c35851fa27
Zeirr <antoine1108@live.fr>
2026-04-14 09:36:47 +0200

    Commit : initialisation page emprunt

---
+++ front/bar-a-livre-front/src/app/page.emprunt/page.emprunt.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page.emprunt',
+  imports: [],
+  templateUrl: './page.emprunt.html',
+  styleUrl: './page.emprunt.css',
+})
+export class PageEmprunt {}
